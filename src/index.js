aedc266e87ad46b11e3d1038cffd5c978480178a
Sido-A <sidonie.abe.uk@gmail.com>
2022-06-26 18:42:17 +0100

    adding plane mesh

--- src/index.js
+++ src/index.js
@@ -1,37 +1,87 @@
 import "./style.css";
 import * as THREE from "three";
-
-//BOX ROTATE
-let scene, mesh, camera, renderer;
-const init = () => {
-  camera = new THREE.PerspectiveCamera(
-    100,
-    window.innerWidth / window.innerHeight,
-    0.01,
-    0
-  );
-  camera.position.z = 1;
-
-  scene = new THREE.Scene();
-
-  const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  const material = new THREE.MeshNormalMaterial();
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setAnimationLoop(animation);
-  document.body.appendChild(renderer.domElement);
+import { OrbitControls } from "./OrbitControls";
+import * as dat from "dat.gui";
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10,
+  },
 };
 
-// animation
-function animation(time) {
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
+gui.add(world.plane, "height", 1, 20, 0.001).onChange(() => refreshGeometry());
+gui.add(world.plane, "width", 1, 20, 0.001).onChange(() => refreshGeometry());
+gui
+  .add(world.plane, "widthSegments", 1, 50, 0.001)
+  .onChange(() => refreshGeometry());
+gui
+  .add(world.plane, "heightSegments", 1, 50, 0.001)
+  .onChange(() => refreshGeometry());
 
-  renderer.render(scene, camera);
+const refreshGeometry = () => {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+  const { array } = planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+    array[i + 2] = z + Math.random();
+  }
+};
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
+// const boxGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+// const boxMaterial = new THREE.MeshNormalMaterial();
+// const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+// scene.add(boxMesh);
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+
+const { array } = planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+
+  array[i + 2] = z + Math.random();
 }
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+const backLight = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, -1);
+scene.add(backLight);
 
 const onWindowResize = () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -39,36 +89,9 @@ const onWindowResize = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 };
 
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
 window.addEventListener("resize", onWindowResize, false);
-
-init();
-
-//LINE
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-// document.body.appendChild(renderer.domElement);
-
-// const camera = new THREE.PerspectiveCamera(
-//   45,
-//   window.innerWidth / window.innerHeight,
-//   1,
-//   500
-// );
-// camera.position.set(0, 0, 100);
-// camera.lookAt(0, 0, 0);
-
-// const scene = new THREE.Scene();
-
-// const material = new THREE.LineBasicMaterial({ color: 0xffffff });
-
-// const points = [];
-// points.push(new THREE.Vector3(-10, 0, 0));
-// points.push(new THREE.Vector3(0, 10, 0));
-// points.push(new THREE.Vector3(10, 0, 0));
-
-// const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-// const line = new THREE.Line(geometry, material);
-
-// scene.add(line);
-// renderer.render(scene, camera);
+animate();
